ff6ca0186a1867c139e1d501b97a625426651706
VASANTH_3105 <vasanth.it2125@gmail.com>
2024-04-13 21:29:05 +0530

    first commit

---
+++ src/test/java/com/bit/bitvbs/BitvbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.bitvbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitvbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
